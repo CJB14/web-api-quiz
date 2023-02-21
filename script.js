7bb4229ae1a7f09f0ec48f5fef9ca848d42a0915
CJB14 <119978168+CJB14@users.noreply.github.com>
2023-02-21 18:32:53 -0500

    updated tezt questions

--- script.js
+++ script.js
@@ -9,15 +9,15 @@ const option_list = document.querySelector(".option_list");
 const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
-// if startQuiz button clicked
+
 start_btn.onclick = ()=>{
 info_box.classList.add("activeInfo"); //show info box
 }
-// if exitQuiz button clicked
+
 exit_btn.onclick = ()=>{
 info_box.classList.remove("activeInfo"); //hide info box
 }
-// if continueQuiz button clicked
+
 continue_btn.onclick = ()=>{
 info_box.classList.remove("activeInfo"); //hide info box
 quiz_box.classList.add("activeQuiz"); //show quiz box
@@ -35,7 +35,7 @@ let counterLine;
 let widthValue = 0;
 const restart_quiz = result_box.querySelector(".buttons .restart");
 const quit_quiz = result_box.querySelector(".buttons .quit");
-// if restartQuiz button clicked
+
 restart_quiz.onclick = ()=>{
 quiz_box.classList.add("activeQuiz"); //show quiz box
 result_box.classList.remove("activeResult"); //hide result box
